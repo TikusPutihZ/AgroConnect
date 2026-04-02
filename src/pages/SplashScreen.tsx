51490127430e395d294c016fed79f8b6ffecbdc6
Amirul Hakimi <147693926+MirulHakim@users.noreply.github.com>
2026-04-02 22:18:19 +0800

    Increase transition duration for splash screen animation

--- src/pages/SplashScreen.tsx
+++ src/pages/SplashScreen.tsx
@@ -9,7 +9,7 @@ const SplashScreen = ({ onFinish }: { onFinish: () => void }) => {
       initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 2.0 }}
+      transition={{ duration: 20.0 }}
       onAnimationComplete={() => {}}
     >
       {/* Watery ripple circles */}
